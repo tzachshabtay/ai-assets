e0c5fec65f70c3fcb11796a5b808c71c849b8d31
Tzach Shabtay <tzachshabtay@Tzachs-MacBook-Neo.local>
2026-06-19 10:02:47 -0500

    Split web and Android demo builds

--- demos/space-invaders/src/main.android.ts
+++ demos/space-invaders/src/main.android.ts
@@ -2,5 +2,6 @@ import { assets } from "./assets.js";
 import { startGame } from "./SpaceInvadersScene.js";
 
 startGame(assets, {
-  assetBaseUrl: new URL(".", globalThis.location.href).href
+  assetBaseUrl: new URL(".", globalThis.location.href).href,
+  targetId: "mobilePortrait"
 });
